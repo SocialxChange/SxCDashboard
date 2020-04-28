bb56110acdc1c4d12c9278625c541c929ff07469
sebacea <seba.cea.echenique@gmail.com>
2020-04-28 15:45:59 -0400

    limpieza y segundo año

--- dev/radarchart.R
+++ dev/radarchart.R
@@ -2,48 +2,43 @@ library(shiny)
 library(plotly)
 library(dplyr)
 
-data <- data.frame(comuna=c('A','B','C', 'D', 'E', 'F'), pobreza=runif(6)*100, desempleo=runif(6)*100)
+data <- data.frame(comuna=LETTERS[1:10], pobreza15=runif(10)*100, pobreza17=runif(10)*100)
 
 ui <- fluidPage(
-  headerPanel('Example'),
+  headerPanel('Pobreza por Comuna 2015-2017'),
   sidebarPanel(
-    selectInput('xcol','X Variable', names(mtcars)),
-    selectInput('ycol','Y Variable', names(mtcars)),
-    selectInput('comunas','Z Variable', levels(data$comuna), selected = levels(data$comuna)[1:3], multiple=TRUE, selectize=TRUE)),
+    selectInput('comunas','Comuna', levels(data$comuna), selected = levels(data$comuna)[1:3], multiple=TRUE, selectize=TRUE)),
   mainPanel(
-    plotlyOutput('plot'),
-    plotlyOutput('plot2')
+    plotlyOutput('plot')
   )
 )
 
 server <- function(input, output) {
   
-  x <- reactive({
-    mtcars[,input$xcol]
-  })
-  
-  y <- reactive({
-    mtcars[,input$ycol]
-  })
+
   
   z <- reactive({
-    data %>% filter(comuna %in% input$comunas) %>% select(pobreza)
+    data %>% filter(comuna %in% input$comunas) %>% select(pobreza15)
+  })
+  
+  k <- reactive({
+    data %>% filter(comuna %in% input$comunas) %>% select(pobreza17)
   })
   
   output$plot <- renderPlotly(
-    plot1 <- plot_ly(
-      x = x(),
-      y = y(), 
-      type = 'scatter',
-      mode = 'markers')
-  )
-  
-  output$plot2 <- renderPlotly(
-    plot1 <- plot_ly(
-      r = as.numeric(unlist(z())),
-      theta = letters[1:length(as.numeric(unlist(z())))], 
+    fig <- plot_ly(
       type = 'scatterpolar',
-      fill = 'toself')
+      fill = 'toself'
+    ) %>%
+      add_trace(
+      r = as.numeric(unlist(z())),
+      theta = input$comunas, 
+      name = '2015')  %>%
+      add_trace(
+        r = as.numeric(unlist(k())),
+        theta = input$comunas, 
+        name = '2017')
+    
   )
   
 }
