4f45d7d06fed2fd7aab2df9924b6339a8fa71d00
sebacea <seba.cea.echenique@gmail.com>
2020-04-28 23:48:52 -0400

    desempeño con indicador responsivo

--- dev/perfomance.R
+++ dev/perfomance.R
@@ -2,14 +2,16 @@ library(shiny)
 library(plotly)
 library(dplyr)
 
-load("~/SxCShinyApp/datos/nominaEmpleabilidad.RData")
-load("~/SxCShinyApp/datos/ComunasAño.RData")
+PathDatos<-Sys.getenv("PathDatos")
+
+load(paste(PathDatos,"nominaEmpleabilidad.RData",sep=""))
+load(paste(PathDatos,"ComunasAño.RData",sep=""))
 
 
 ui <- fluidPage(
-  headerPanel('Pobreza por Comuna 2015-2017'),
+  headerPanel('Desempeño esperado beneficiarios 2015-2017'),
   sidebarPanel(
-    selectInput('comunasPer','Comuna', levels(nomina$Localidad), selected = levels(nomina$Localidad)[1], multiple=TRUE, selectize=FALSE)),
+    selectInput('indicador','Métrica', c("empleabilidad", "continuidad"), selected = "empleabilidad", multiple=FALSE, selectize=FALSE)),
   mainPanel(
     plotlyOutput('plot')
   )
@@ -20,27 +22,26 @@ server <- function(input, output) {
 
   
   z <- reactive({
-    nomina %>% filter(comuna %in% input$comunasPer) %>% select(`Empleabilidad 1er año`)
+    nomina %>% filter(comuna %in% input$comunasPer) %>% select(empleabilidad)
   })
   
   k <- reactive({
-    nomina %>% filter(comuna %in% input$comunasPer) %>% select(`% titulados con continuidad de estudios`)
+    nomina %>% filter(comuna %in% input$comunasPer) %>% select(continuidad)
   })
   
   output$plot <- renderPlotly(
-    fig <- plot_ly(
-      type = 'scatterpolar',
-      fill = 'toself'
-    ) %>%
-      add_trace(
-        r = as.numeric(unlist(z())),
-        theta = input$comunas, 
-        name = '2015')  %>%
-      add_trace(
-        r = as.numeric(unlist(k())),
-        theta = input$comunas, 
-        name = '2017')
-    
+    fig <- plot_ly(x = ~as.numeric(unlist(aggcomunas %>% filter(comuna=="Baquedano") %>% select(Año))), 
+                   y = ~as.numeric(unlist(aggcomunas %>% filter(comuna=="Baquedano") %>% select(input$indicador))), 
+                   name = 'Baquedano', type = 'scatter', mode = 'lines', fill = 'tonexty',fillcolor = '#5BCBFD') %>% 
+      add_trace(x = ~as.numeric(unlist(aggcomunas %>% filter(comuna=="Sierra Gorda") %>% select(Año))), 
+                y = ~as.numeric(unlist(aggcomunas %>% filter(comuna=="Sierra Gorda") %>% select(input$indicador))), 
+                name = 'Sierra Gorda', fillcolor = '#EFC000') %>% 
+      layout(title = 'Desempeño esperado beneficiarios',
+             xaxis = list(title = "Año",
+                          showgrid = FALSE),
+             yaxis = list(title = input$indicador,
+                          showgrid = FALSE,
+                          ticksuffix = ''))
   )
   
 }
