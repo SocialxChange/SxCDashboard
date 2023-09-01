5dcf4bf7cc7a8ac96724f99871dd6e51aefe759c
mavargas11 <mavargas11@uc.cl>
2023-09-01 09:03:38 -0700

    saca todos los warnings

---
+++ R/sxcdashboard-package.R
@@ -0,0 +1,117 @@
+#' Colunga
+#' 
+#' Explicar en que consiste esta tabla
+#' 
+#' @source U de los Andes?
+#' @format ## `colunga`
+#' A data frame with 174 rows and 41 columns
+#'
+"colunga"
+
+#' Datos 1
+#' 
+#' Explicar en que consiste este vector
+#' 
+#' @source U de los Andes?
+#' @format ## `datos1`
+#' A vector with 20 elements
+"datos1"
+
+#' Datos 2
+#' 
+#' Explicar en que consiste este vector
+#' 
+#' @source U de los Andes?
+#' @format ## `datos2`
+#' A vector with 20 elements
+"datos2"
+
+#' Datos 3
+#' 
+#' Explicar en que consiste este vector
+#' 
+#' @source U de los Andes?
+#' @format ## `datos3`
+#' A vector with 20 elements
+"datos3"
+
+#' Datos 4
+#' 
+#' Explicar en que consiste este vector
+#' 
+#' @source U de los Andes?
+#' @format ## `datos4`
+#' A vector with 20 elements
+"datos4"
+
+#' Datos 5
+#' 
+#' Explicar en que consiste este vector
+#' 
+#' @source U de los Andes?
+#' @format ## `datos5`
+#' A vector with 20 elements
+"datos5"
+
+#' Datos 6
+#' 
+#' Explicar en que consiste este vector
+#' 
+#' @source U de los Andes?
+#' @format ## `datos6`
+#' A vector with 20 elements
+"datos6"
+
+#' Donaciones Mallplaza
+#' 
+#' Explicar en que consiste esta tabla
+#' 
+#' @source U de los Andes?
+#' @format ## `donaciones_mallplaza`
+#' A data frame with 13 rows and 11 columns
+"donaciones_mallplaza"
+
+#' Gtrends
+#' 
+#' Explicar en que consiste esta tabla
+#' 
+#' @source U de los Andes?
+#' @format ## `gtrends`
+#' A data frame with 28 rows and 6 columns
+"gtrends"
+
+#' Metricas Comuna
+#' 
+#' Explicar en que consiste esta tabla
+#' 
+#' @source U de los Andes?
+#' @format ## `metricas_comuna`
+#' A data frame with 11 rows and 3 columns
+"metricas_comuna"
+
+#' Proyectos 2016
+#' 
+#' Explicar en que consiste esta tabla
+#' 
+#' @source U de los Andes?
+#' @format ## `proyectos_2016`
+#' A data frame with 65 rows and 12 columns
+"proyectos_2016"
+
+#' Tot Categorias
+#' 
+#' Explicar en que consiste esta tabla
+#' 
+#' @source U de los Andes?
+#' @format ## `tot_categorias`
+#' A data frame with 10 rows and 2 columns
+"tot_categorias"
+
+#' Variables
+#' 
+#' Explicar en que consiste esta tabla
+#' 
+#' @source U de los Andes?
+#' @format ## `variables`
+#' A vector with 21 elements
+"variables"
